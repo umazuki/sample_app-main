f32ad88998bb3feba6f64886a83964616ae05acf
umazuki <chill0715rain@gmail.com>
2023-12-21 12:20:29 +0000

    modify micropost_controller_test

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,64 @@
+module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    session[:session_token] = user.session_token
+  end
+
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+   # 現在ログイン中のユーザーを返す（いる場合）
+   def current_user
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
+  # ユーザーがログインしていればtrue, その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    reset_session
+    @current_user = nil
+  end
+
+  # アクセスしようとしたURLを保存する
+  # request.original_url: リクエスト先を取得できる
+  # session変数の:forwarding_urlキーに保存する（GETリクエストが送られた時のみ）
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "chill0715rain@gmail.com"
+  layout "mailer"
+end
